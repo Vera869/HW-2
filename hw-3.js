1f2c60e5d682401c3074133ca14bad5021789277
Vera869 <verea8910@gmail.com>
2023-08-22 12:12:49 +0700

    Доработала всё кроме 7 задания

--- hw-3.js
+++ hw-3.js
@@ -2,23 +2,26 @@
 let password = 'простой пароль';
 let aske = String(prompt("Введите пароль"));
 aske.toLowerCase();
-if (password === 'простой пароль') {
+if (password === aske) {
    alert("Пароль введен верно");
 } else {
    alert("Пароль введен неправильно")
 }                                          
  //Задание 2
+
 let c = Number(prompt("Введите число"));   //проверить при c равной 0, 10, −3, 2.
-c = (c>0 && c<10) ? console.log(alert("Верно")): console.log(alert("Неверно")); 
+c = (c>0 && c<10) ? console.log("Верно"): console.log("Неверно"); 
 //Задание 3  
+
 let d = Number(prompt("Введите число"));
 let e = Number(prompt("Введите число"));
-if (d===100 || e===100) {
-   console.log(alert("Верно"));
+if (d>100 || e>100) {
+   console.log("Верно");
 } else {
-   console.log(alert("Неверно"));
+   console.log("Неверно");
 }                
-//Задание 4                    
+//Задание 4     
+                
 let a = '2';
 let b = '3';
 alert(Number(a) + Number(b));           
@@ -26,42 +29,18 @@ alert(Number(a) + Number(b));
 let monthNumber = Number(prompt("Укажите номер месяца"));
 
 switch (monthNumber) {
-   case 1:
+   case 1, 2, 12:
       alert("Зимушка-зима");
       break;
-   case 2:
-      alert("Зимушка-зима");
-      break;
-   case 3:
+   case 3, 4, 5:
       alert("Весна-красна");
       break;
-   case 4:
-      alert("Весна-красна");
-      break;
-   case 5:
-      alert("Весна-красна");
-      break;
-   case 6:
+   case 6, 7, 8:
       alert("Лето!!!");
       break;   
-   case 7:
-      alert("Лето!!!");
-      break;
-   case 8:
-      alert("Лето!!!");
-      break;
-   case 9:
+   case 9, 10, 11:
       alert("Осень- прекрасная пора, очей очарованье");
       break;
-   case 10:
-      alert("Осень- прекрасная пора, очей очарованье");
-      break;
-   case 11:
-      alert("Осень- прекрасная пора, очей очарованье");
-      break;
-   case 12:
-      alert("Зимушка-зима");
-      break;         
    default:
       alert("нет такого месяца");
       break;
@@ -70,14 +49,18 @@ switch (monthNumber) {
 //Дополнительная практика
 
 //Задание 7
-let variable = Number(prompt("Пожалуйста, введите любое число"));
+
+let variable = prompt("Пожалуйста, введите любое число");
 let division = variable % 2;
 let result = division===0 ? alert("Число четное"): alert("Число нечетное"); 
 
+
 //Задание 8
+
 let clientOS = Number(prompt("Если Ваша  операционная система ios-введите 0, если Androin-введите 1"))
 let answer = clientOS === 0 ? console.log("Установите версию приложения для iOS по ссылке") : console.log("Установите версию приложения для Android по ссылке");
 //Задание 9
+
 let clientDeviceYear = Number(prompt("Укажите год выпуска Вашего устройства"));
 
 if (clientOS === 0) {
@@ -94,6 +77,4 @@ if (clientOS === 0) {
       console.log("Установите облегченную версию приложения для Android по ссылке");
    } 
 }
-
-
-
+*/
